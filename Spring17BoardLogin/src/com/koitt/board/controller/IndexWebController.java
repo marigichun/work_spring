54eb86321801d4c34c4f972c73ed0521da5332b0
KOITT <KOITT@KOITT-PC04>
2018-02-26 17:48:07 +0900

    Spring Dao Test = Test Driven Development - JUnit Libraries 사용(4 버전) (개발 과정중 중간에 테스트후 개발하는 과정)

--- Spring17BoardLogin/src/com/koitt/board/controller/IndexWebController.java
+++ Spring17BoardLogin/src/com/koitt/board/controller/IndexWebController.java
@@ -1,9 +1,6 @@
 package com.koitt.board.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,24 +9,8 @@ import com.koitt.board.service.UsersService;
 @Controller
 public class IndexWebController {
 	
-	@Autowired
-	private UsersService service;
-	
 	@RequestMapping(value= {"/", "/index.do"}, method=RequestMethod.GET)
-	public String index(Model model) {
-		
-		// UsersService의 principal 객체를 가져온다.
-		UserDetails principal = service.getPrincipal();
-		
-		// principal 객체가 존재한다는 것은 로그인 된 상태
-		if (principal != null) {
-			model.addAttribute("isLogin", true);
-		}
-		// 그렇지 않다면 로그인이 되지 않은 상태
-		else {
-			model.addAttribute("isLogin", false);
-		}
-		
+	public String index() {
 		return "index";
 	}
 }
