4444b53db4dd0437188b0f30561dc04faa178c74
KOITT <KOITT@KOITT-PC04>
2018-03-02 17:51:20 +0900

    자바 추가 및 Spring Etc04 cron 작성 예제

---
+++ SpringEtco4Task/src/com/koitt/test/TestDrive.java
@@ -0,0 +1,10 @@
+package com.koitt.test;
+
+public class TestDrive {
+	
+	public static void main(String[] args) {
+		ApplicationContext context =
+				new ClassPathXmlApplicationContext("com/koitt/cocfing/config.xml");
+	}
+	
+}	
